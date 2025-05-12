97af36a720a9127e272745057d31b936aef6e771
ernesto-official02 <ernestanmol@gmail.com>
2025-05-12 20:38:29 +0530

    update

--- frontend/src/component/Hero.jsx
+++ frontend/src/component/Hero.jsx
@@ -35,7 +35,6 @@ const Hero = () => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
           <span className="font-semibold">30-Minute Service Guarantee</span>
-          <h1>hii every on </h1>
         </div>
       </div>
 
